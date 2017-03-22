125d48db780c12e16eb39f161446f843befac480
Iris Faraway <iris.faraway@gmail.com>
2017-03-22 14:09:49 +0000

    add basic tests and features

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,15 @@
+# Calculate strings
 class StringCalculator
+  def self.add(input)
+    if input.empty?
+      0
+    else
+      numbers = input.split(',')
+      sum = 0
+      numbers.each do |number|
+        sum += number.to_i
+      end
+      sum
+    end
+  end
 end
